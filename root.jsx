4de4cd03d55dc993400a702519f71eec5cde55b3
Maria Belgrader <mbelgrader5@gmail.com>
2016-12-25 12:50:14 -0800

    Initial project setup

---
+++ root.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Root extends React.Component{
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Hello World</h1>
+
+      </div>
+    );
+  }
+}
+
+export default Root;
